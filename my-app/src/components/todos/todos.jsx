7a1d5671fbd7d18747638da588b91457e4a59010
Grant <gmontgomery1994@gmail.com>
2019-10-29 12:56:22 -0900

    basic functionality finally working

--- my-app/src/components/todos/todos.jsx
+++ my-app/src/components/todos/todos.jsx
@@ -7,8 +7,7 @@ class ToDos extends Component {
     constructor(props){
         super(props)
         this.state = {todos: [
-            {id: 1, value: "Wash Dishes."},
-            {id: 2, value: "Take out trash."}
+            
         ],
         newTodo: {id: null, value: ""}}
     }
@@ -16,21 +15,19 @@ class ToDos extends Component {
         this.setState(
             {todos: this.state.todos.filter(todo => todo.id !== todoid)}
         )
-        console.log(this.state.newTodo)
     }
     addTask = () => {
         this.setState({
             todos: [...this.state.todos, this.state.newTodo]
         })
         this.setState({newTodo: {id: null, value: ""}})
-        console.log(...this.state.todos)
-       
     }
     updateInput = newinput => {
-        this.setState({newTodo: {id: this.state.todos.length + 1, task: newinput}})
-        console.log(this.state.newTodo)   
+        this.setState({newTodo: {id: Math.floor(Math.random() * Math.floor(100)), value: newinput}})   
      }
     render() { 
+        console.log(this.state.todos)
+        console.log(this.state.newTodo)
         return (
         <React.Fragment>
             <Input updateInput={this.updateInput} addTask={this.addTask}/>
